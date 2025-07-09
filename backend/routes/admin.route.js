c1fda609e4897a55480eb1069838dafa236da08b
inornnut01 <inornnut01@gmail.com>
2025-07-09 09:51:02 +0700

    backend - Album Routes & controller

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import {
+  createSong,
+  deleteSong,
+  createAlbum,
+  deleteAlbum,
+  checkAdmin,
+} from "../controllers/admin.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -13,12 +15,23 @@ import statRoutes from "./routes/stat.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(clerkMiddleware()); // this will add auth to req obj => req.auth.userId
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 1024 * 1024 * 10, // 10MB
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -26,6 +39,18 @@ app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+//error handle
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal server error"
+          : err.message,
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
